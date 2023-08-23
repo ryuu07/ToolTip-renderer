2cab55489b31d67ba48ba8f93c279c751de96b0a
Pranav S <pranav.s2020a@vitstudent.ac.in>
2023-08-23 18:56:24 +0530

    update

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -66,7 +66,7 @@ export const TooltipItem = ({
                 (position === "left" &&
                   `absolute right-full top-1/2 z-20 mr-3 -translate-y-1/2 whitespace-nowrap rounded   font-semibold  opacity-0 lg:group-hover:opacity-100 sm:group-hover:opacity-100 sm:group-focus:opacity-0`) ||
                 (position === "bottom" &&
-                  `absolute top-full left-1/2 z-20 mt-3 -translate-x-1/2 whitespace-nowrap rounded   font-semibold  opacity-0 lg:group-hover:opacity-100 sm:group-hover:opacity-100 sm:group-focus:opacity-0 `)
+                  `absolute top-full left-1/2 z-20 mt-3 -translate-x-1/2 whitespace-nowrap rounded   font-semibold  opacity-0 lg:group-hover:opacity-100 sm:group-hover:opacity-100 sm:group-focus:opacity-100 `)
               }`}
             >
               <span
